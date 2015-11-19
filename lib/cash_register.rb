b8b15a2d1e38bd00972f3b9e39ce476f6ad6d9a0
Justin Thomas <jthomas0209@gmail.com>
2015-11-19 14:26:56 -0500

    Defined methods/variables in class CashRegister

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,33 @@
+class CashRegister
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+    @prev_price = 0
+  end
 
+  attr_accessor :total
+  attr_accessor :discount
+  attr_accessor :items
+
+  def add_item(item,price,quantity=1)
+    @prev_price = @total.to_f
+    @total += (price * quantity)
+    @items += [item]*quantity
+  end
+
+  def apply_discount
+    if @discount == 0
+      output = "There is no discount to apply."
+    else
+      @total *= 1-@discount/100.to_f
+      @total = @total.to_i
+      output =  "After the discount, the total comes to $#{total}."
+    end
+  end
+
+  def void_last_transaction
+    @total = @prev_price.to_f
+    @items.pop
+  end
+end
