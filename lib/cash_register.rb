6cf360da7f6deecce89fe1c73c606286798f52ed
dannyd4315 <dannyd4315@gmail.com>
2015-11-25 15:05:42 +0000

    add_item and initialize method

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,19 @@
+class CashRegister
 
+  attr_accessor :total, :discount
+
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+  end 
+
+  def add_item(item, price, quantity=1) 
+    self.total += price * quantity
+  end
+
+  def apply_discount
+    
+  end  
+
+
+end
