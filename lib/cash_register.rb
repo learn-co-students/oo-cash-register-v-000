c763f379c342c64d7c4ad43651721b55f9ec9d84
eric-an <erican@me.com>
2015-12-26 02:23:03 -0800

    build an OO cash register

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,39 @@
+require 'pry'
 
+class CashRegister
+  attr_accessor :total, :discount, :items, :last_transaction
+
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def self.total
+    @total
+  end
+
+  def add_item(item, price=0, quantity=1)
+    quantity.times { @items << item }
+    self.total += price * quantity
+    self.last_transaction = price * quantity
+  end
+
+  def apply_discount
+    if @discount == 0
+      "There is no discount to apply."
+    else
+      self.total -= (self.total * (self.discount/100.0))
+      "After the discount, the total comes to $#{self.total.to_i}."
+    end
+  end
+
+  def items
+    @items
+  end
+
+  def void_last_transaction
+    self.total -= self.last_transaction
+  end
+
+end
