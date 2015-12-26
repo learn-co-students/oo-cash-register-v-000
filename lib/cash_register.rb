bc2e5f3aac7b771a09b61d25b960c66bfe713730
preetdha <manpreet.dha@gmail.com>
2015-12-26 12:51:17 -0800

    built oo cash register

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,40 @@
+class CashRegister
 
+attr_accessor :total, :discount, :items, :last_transaction
+
+def initialize(discount = nil)
+  @total = 0
+  @discount = discount
+  @items = []
+end
+
+def total
+  @total
+end
+
+def add_item(title, price, quantity = 1)
+  self.total += (price * quantity)
+  quantity.times do 
+    @items << title 
+    end
+    @last_transaction = self.total
+end
+
+def apply_discount
+    if discount != nil
+    self.total -= (total * (discount / 100.0))
+    "After the discount, the total comes to $#{self.total.to_i}."
+    else
+    "There is no discount to apply."
+  end
+end
+
+def items
+  @items
+end
+
+def void_last_transaction
+  self.total = 0
+end
+
+end
