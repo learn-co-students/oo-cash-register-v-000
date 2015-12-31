8b8ed8082f5860ab0ae8c3557db77b82e967872b
Chris Bruen <bruen.chris@gmail.com>
2015-12-31 15:34:41 -0500

    again

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,6 @@
 class CashRegister
 	attr_reader :discount, :items
+	attr_accesor :total
 	def initialize(discount=0)
 		@total = 0
 		@discount = discount
@@ -15,8 +16,9 @@ class CashRegister
 	
 	def apply_discount
 		if @disount != 0
-			@total = @total * (1-@discount/100)
-			puts "After the discount, the total comes to #{@discount}."
+			@total = @total * (1.to_f-@discount.to_f/100.to_f)
+			@total = @total.to_i
+			puts "After the discount, the total comes to #{@total}."
 		else
 			puts "There is no discount to apply."
 		end
