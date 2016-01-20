e843d9289169991b977f3dae0660fa6227b1e0be
thegands <thegands@gmail.com>
2016-01-19 19:57:50 -0700

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,28 +1,31 @@
 class CashRegister
 
-  attr_accessor :total, :discount, :items, :last_transaction
+  attr_accessor :items, :discount, :total, :last_transaction
 
-  def initialize(discount = nil)
-    self.total = 0
-    self.discount = discount
-    self.items = []
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @items = []
   end
 
-  def add_item(title, price, quantity = 1)
-    self.total += price * quantity
-    quantity.times { self.items << title }
-    self.last_transaction = [title, price, quantity]
+  def add_item(title, amount, quantity=1)
+    self.total += amount * quantity
+    quantity.times do
+      items << title
+    end
+    self.last_transaction = amount * quantity
   end
 
   def apply_discount
-    self.total == 0 ? "There is no discount to apply." : "After the discount, the total comes to $#{self.total = self.total * 8 / 10 }."
-  end
-
-  def void_last_transaction
-    self.total -= self.last_transaction[1] * self.last_transaction[2]
-    self.last_transaction[2].times do
-      self.items.delete_at(self.items.index(self.last_transaction[0]) || self.items.count)
+    if discount != 0
+      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+      "After the discount, the total comes to $#{self.total}."
+    else
+      "There is no discount to apply."
     end
   end
 
+  def void_last_transaction
+    self.total = self.total - self.last_transaction
+  end
 end
