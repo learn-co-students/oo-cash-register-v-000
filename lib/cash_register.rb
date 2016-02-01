a2aae7e2fff7346c3561916cf94e76bb5e42b0eb
zachnewburgh <zach.newburgh@gmail.com>
2016-02-01 15:25:54 -0500

    cash register that adds items, applies staff discount, prints receipt, and voids last transaction

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,39 @@
+require 'pry'
 
+class CashRegister
+  attr_accessor :total, :discount, :items, :last_transaction
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(item, price, quantity = 1)
+    self.total += (price * quantity)
+    counter = 0
+    while quantity > counter
+      @items << item
+      counter += 1
+    end
+    self.last_transaction = price * quantity
+  end
+
+  def apply_discount
+    if self.discount > 0
+      self.total = (self.total * (1-(self.discount * 0.01))).round
+      return "After the discount, the total comes to $#{self.total}."
+    else
+      return "There is no discount to apply."
+    end
+  end
+
+  def items
+    @items
+  end
+
+  def void_last_transaction
+    self.total -= self.last_transaction
+  end
+
+end
