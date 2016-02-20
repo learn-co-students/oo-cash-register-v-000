711e7298eb5a5b0ad13c7de82e4bea04f2322aa7
hide83 <hiral.desai83@gmail.com>
2016-02-20 05:15:57 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
+require 'pry'
+class CashRegister
 
+  attr_accessor :total, :discount, :title, :price, :optional_quantity
+
+  def initialize(discount=0)
+    @total=0
+    @discount=discount
+  end
+
+  def add_item(title,price,optional_quantity=1)
+    @optional_quantity=optional_quantity
+    @total += price*optional_quantity
+   end
+
+  def apply_discount
+    @total=price-discount
+    binding.pry
+  end
+end
