d78f451512760646cb7fccfa9e3ec2fa0ccb3196
hide83 <hiral.desai83@gmail.com>
2016-02-22 06:29:28 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,61 +1,31 @@
-require 'pry'
 class CashRegister
 
-  attr_accessor :total, :discount, :title, :apply_discount, :quantity, :void_last_transaction
+  attr_accessor :items, :discount, :total, :last_transaction
 
   def initialize(discount=0)
-    @total=0
-    @discount=discount
-    @cart=[]
+    @total = 0
+    @discount = discount
+    @items = []
   end
 
-  def add_item(title,price,quantity=1)
-    item_all={}
-    item_all[:item_name]=title
-   item_all[:price]=price
-    item_all[:quantity]=quantity
-
-    @cart << item_all
-
-    @total += price*quantity
-
-   end
+  def add_item(title, amount, quantity=1)
+    self.total += amount * quantity
+    quantity.times do
+      items << title
+    end
+    self.last_transaction = amount * quantity
+  end
 
   def apply_discount
-
-   if @discount!=0
-   @total= self.total-discount*10
-    "After the discount, the total comes to $#{@total}."
-     else
-     "There is no discount to apply."
-   end
+    if discount != 0
+      self.total = (total * ((100.0 - discount.to_f)/100)).to_i
+      "After the discount, the total comes to $#{self.total}."
+    else
+      "There is no discount to apply."
+    end
   end
 
-
-  def items
-   @items=[]
-    @cart.collect do |item_all|
-      for quantity in 1..item_all[:quantity]
-      #item_all.each do |qty|
-       #while qty=1 && qty==item_all[:quantity]
-
-    @items << item_all[:item_name]
-
-    # @item_all.collect{|name| name==item_name}
-    #@cart.each{|name| name== title}
-    #@cart
-
-      end
-  end
-     @items
-  end
   def void_last_transaction
-      #@cart.each do |item_all|
-
-
-      #item_all[:price].collect do|prices|
-       # prices==item_all[:price].length
-      #@void_last_transaction = @total-self.total.last
-
+    self.total = self.total - self.last_transaction
   end
 end
