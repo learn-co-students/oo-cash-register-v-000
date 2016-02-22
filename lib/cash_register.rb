61441ba2a264afe57f10af350ba8c3751b2ad2a9
hide83 <hiral.desai83@gmail.com>
2016-02-22 06:14:55 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,20 +1,61 @@
 require 'pry'
 class CashRegister
 
-  attr_accessor :total, :discount, :title, :price, :optional_quantity
+  attr_accessor :total, :discount, :title, :apply_discount, :quantity, :void_last_transaction
 
   def initialize(discount=0)
     @total=0
     @discount=discount
+    @cart=[]
   end
 
-  def add_item(title,price,optional_quantity=1)
-    @optional_quantity=optional_quantity
-    @total += price*optional_quantity
+  def add_item(title,price,quantity=1)
+    item_all={}
+    item_all[:item_name]=title
+   item_all[:price]=price
+    item_all[:quantity]=quantity
+
+    @cart << item_all
+
+    @total += price*quantity
+
    end
 
   def apply_discount
-    @total=price-discount
-    binding.pry
+
+   if @discount!=0
+   @total= self.total-discount*10
+    "After the discount, the total comes to $#{@total}."
+     else
+     "There is no discount to apply."
+   end
+  end
+
+
+  def items
+   @items=[]
+    @cart.collect do |item_all|
+      for quantity in 1..item_all[:quantity]
+      #item_all.each do |qty|
+       #while qty=1 && qty==item_all[:quantity]
+
+    @items << item_all[:item_name]
+
+    # @item_all.collect{|name| name==item_name}
+    #@cart.each{|name| name== title}
+    #@cart
+
+      end
+  end
+     @items
+  end
+  def void_last_transaction
+      #@cart.each do |item_all|
+
+
+      #item_all[:price].collect do|prices|
+       # prices==item_all[:price].length
+      #@void_last_transaction = @total-self.total.last
+
   end
 end
