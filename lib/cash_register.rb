b97563c848962ecb0f0b3af818b10f4d97b65dfd
jbok4 <jbok4@aol.com>
2016-03-02 14:59:56 -0500

    this lab was fun

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,43 @@
+class CashRegister
 
+  attr_accessor :total, :discount, :price, :title, :last_transaction
+
+  
+
+  def initialize(discount = 0)
+    self.discount = discount
+    @total = 0
+    @array = []
+    @last_transaction = 0
+  end
+
+  def total
+    @total
+  end
+
+  def add_item(title, price, quantity = 1)
+    self.total = (price * quantity) + self.total
+    quantity.times do
+    @array.push(title)
+    @last_transaction = self.total
+    end
+  end
+
+  def apply_discount
+    if self.discount == 0
+      return "There is no discount to apply."
+    else
+    @total = self.total - (self.discount * 10)
+    return "After the discount, the total comes to $#{@total}."
+    end
+  end
+
+  def items
+    @array
+  end
+
+  def void_last_transaction
+    @total = @total - @last_transaction
+  end
+
+end
