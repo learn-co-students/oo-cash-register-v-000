05b5f82d45439f15e5f4649f02d7123d027a09b6
Christian Whitesides <whitesidescr@gmail.com>
2016-04-08 19:24:27 -0600

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,42 +1,54 @@
 class CashRegister
-  attr_accessor :total
+  attr_accessor :items, :discount, :total, :last_transaction
 
   def initialize(discount=0)
     @total = 0
     @discount = discount
     @items = []
-    @price_of_last_product = 0
+    # @price_of_last_product = 0
   end
 
-  def discount
-    @discount
-  end
+  ## Just put it in an attr_accessor -- duh.
+  # def discount
+  #   @discount
+  # end
 
   def add_item(title, price, quantity=1)
-    @total += price * quantity
-    @price_of_last_product = price
+    self.total += price * quantity
 
-    counter = quantity
-    while counter > 0
-      self.items << title
-      counter -= 1 
+    # counter = quantity
+    # while counter > 0
+    #   self.items << title
+    #   counter -= 1 
+    # end
+    quantity.times do 
+      items << title
     end
+    self.last_transaction = price * quantity
   end
 
   def apply_discount
-    @total -= @total * self.discount/100
-    if self.discount == 0
-      "There is no discount to apply."
+    # @total -= @total * self.discount/100
+    # if self.discount == 0
+    #   "There is no discount to apply."
+    # else
+    #   "After the discount, the total comes to $#{@total}."
+    # end 
+    if discount != 0
+      self.total -= self.total * discount/100
+      "After the discount, the total comes to $#{self.total}."
     else
-      "After the discount, the total comes to $#{@total}."
-    end 
+      "There is no discount to apply."
+    end
   end
 
-  def items
-    @items
-  end
+  ## Just put it in an attr_accessor -- duh.
+  # def items
+  #   @items
+  # end
 
   def void_last_transaction
-    @total -= @price_of_last_product
+    # @total -= @price_of_last_product
+    self.total = self.total - self.last_transaction
   end
 end
