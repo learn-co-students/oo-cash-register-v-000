90a75eafc21719f7364454daeeb98a7b6e798541
Michael Randell <michael.james.randell@gmail.com>
2016-04-22 13:33:56 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,17 @@
+class CashRegister
+  attr_accessor :discount
 
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+  end
+
+  def discount(discount)
+    @discount += discount
+  end
+
+
+
+
+
+end
