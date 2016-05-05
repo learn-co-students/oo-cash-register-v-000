24bddbbfba50a9ca3c7fda603e7ea165d29df199
kaileeagray <kaileeagray@gmail.com>
2016-05-04 20:06:25 -0500

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,17 +4,17 @@ class CashRegister
 
   def initialize(num = 0)
     @total = 0
-    num != 0 ?
-    @discount = num : @discount = 0
+    @discount = num
+    self.apply_discount
   end
 
-  # def total=(num)
-  #   if @discount = 0
-  #     @total = num
-  #   else
-  #     @total = @total*((100-@discount)/100)
-  #   end
-  # end
+  def apply_discount
+    if @discount = 0
+      @total = num
+    else
+      @total = @total*((100-@discount)/100)
+    end
+  end
 
   def add_item(item, cost, quantity = 1)
     @total += cost*quantity
