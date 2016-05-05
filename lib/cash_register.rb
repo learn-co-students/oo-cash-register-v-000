da8126ae0eff9278e18911e33865a44d3680524c
kaileeagray <kaileeagray@gmail.com>
2016-05-04 20:04:22 -0500

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,23 @@
+class CashRegister
+  attr_accessor :total
+  attr_reader :discount
 
+  def initialize(num = 0)
+    @total = 0
+    num != 0 ?
+    @discount = num : @discount = 0
+  end
+
+  # def total=(num)
+  #   if @discount = 0
+  #     @total = num
+  #   else
+  #     @total = @total*((100-@discount)/100)
+  #   end
+  # end
+
+  def add_item(item, cost, quantity = 1)
+    @total += cost*quantity
+  end
+
+end
