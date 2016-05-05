24531de991d25db52cabfa08058b09a329990f67
kaileeagray <kaileeagray@gmail.com>
2016-05-04 21:48:39 -0500

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,18 +1,30 @@
 class CashRegister
   attr_accessor :total
-  attr_reader :discount
+  attr_reader :discount, :items
 
   def initialize(num = 0)
     @total = 0
-    @discount = num
+    @discount = num.to_f
+    @items = []
   end
 
-  def self.apply_discount
-    @total -= @total*(@discount/100)
+  def apply_discount
+    @total = @total.to_f*((100-@discount)/100)
+    @discount == 0 ?
+    "There is no discount to apply." : "After the discount, the total comes to $#{@total.round}."
   end
 
   def add_item(item, cost, quantity = 1)
+    @last_item = item
+    @last_item_quantity = quantity
+    @last_item_cost = cost*quantity
     @total += cost*quantity
+    @items.concat([item]*quantity)
+  end
+
+  def void_last_transaction
+    @total -= @last_item_cost
+    @items.pop(@last_item_quantity)
   end
 
 end
