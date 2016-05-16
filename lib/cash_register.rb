fef389c8ae7d7abe5fd0b3c9fa3f1202e983e882
Jonathan Stone <jon_p_stone@yahoo.com>
2016-05-16 09:55:06 -0500

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,14 @@
+class CashRegister
 
+  attr_accessor :total
+
+  def initialize(discount = 0)
+    @discount = discount
+    @total = 0
+  end
+
+  def cash_register_with_discount
+    @total - @discount
+  end
+
+end
