ca183c629ac21aba861e256655340eb6ac6c9a36
ndaniele <n.daniele@gmail.com>
2016-05-19 16:15:48 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
+require "pry"
+class CashRegister
+  attr_accessor :discount, :total
 
+  def initialize(*discount)
+     @discount = discount.join.to_i
+     @total = 0
+  end
+
+  def add_item(title, price, *quantity)
+    @total = total + price * quantity
+
+  end
+
+
+  #def add_item(title, price, *quantity)
+  #  price = price.to_f
+  ##  quantity = quantity.join.to_i
+  #  @total = @total + price * quantity
+#
+ # end
+
+end
+
+ 
