ae095d93b32eab20da7690d196da0e1209439121
wedesignstudios <deelden@gmail.com>
2016-05-19 16:26:27 -0500

    add methods to cash register class

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,32 @@
+class CashRegister
 
+  attr_accessor :total, :discount, :all_items, :tally
+
+  def initialize(disc=0)
+    @total = 0
+    @discount = disc.to_f
+    @all_items = []
+    @tally = []
+  end
+
+  def add_item(title, price, quantity=1)
+    @all_items << title.split * (quantity)
+    total_price = price.to_f * quantity.to_i
+    @tally << total_price
+    @total += total_price
+  end
+
+  def apply_discount
+    @total = @total - @discount/100 * @total
+    @discount > 0 ? "After the discount, the total comes to $#{@total.to_i}." : "There is no discount to apply."
+  end
+
+  def items
+    @all_items.flatten
+  end
+
+  def void_last_transaction
+    @total = @total - @tally.last
+  end
+
+end
