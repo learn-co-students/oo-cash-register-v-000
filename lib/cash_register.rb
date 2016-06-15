2459beecd84352357f84dbed74a85f2b20080b48
Lenn Hypolite <lenntriniman@hotmail.com>
2016-06-15 23:38:12 +0000

    initial commit

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,35 @@
+require "pry"
 
+class CashRegister
+
+  #returns the current total
+  attr_accessor :total, :quantity
+
+  #define the discount instance
+  attr_reader :discount, :title, :price
+
+  def initialize (discount='')
+    @total = 0
+    @discount = 20
+  end
+
+  def add_item(title, price, quantity=1)
+    if quantity > 0  #if quantity is added then execute to following code
+      self.total += (price*quantity)
+    else
+      self.total +=price #else execute this
+    end
+  end
+
+  def apply_discount
+   self.add_item(title, price, quantity=1)
+  end
+
+  def items
+
+  end
+
+  def void_last_transaction
+
+  end
+end
