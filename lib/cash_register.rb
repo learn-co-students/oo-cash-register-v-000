de44984a8f18590de40f6217c02e2281d36142b3
Michael Stewart <upp_mike@yahoo.com>
2016-06-16 18:53:23 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,34 @@
+#require 'pry'
 
+class CashRegister
+  attr_accessor :total, :discount, :item, :items, :last_transaction
+
+  def initialize(discount = nil)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(item, price, quantity = 1)
+    @item = item
+    #binding.pry
+    quantity.times do
+      @items << item
+    end
+    @total += price * quantity
+    @last_transaction = price * quantity
+  end
+
+  def apply_discount
+    if @discount
+      @total = @total - (@total * @discount / 100)
+      "After the discount, the total comes to $#{@total}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def void_last_transaction
+    @total = @total - @last_transaction
+  end
+end
