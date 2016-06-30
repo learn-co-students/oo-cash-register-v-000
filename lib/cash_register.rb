1bd273c1062eee4e6ebec4a8338c4487b80b7336
vicision <duncan.j.mclachlan@gmail.com>
2016-06-30 16:25:12 -0400

    Failing test: pushing for help

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
 
+class CashRegister
+
+  attr_accessor :total, :discount
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+
+
+  def add_item(title, price, quantity = 0)
+    quantity > 0 ? @total += price * quantity : @total += price
+  end
+
+  def apply_discount
+      @total = @total - @discount
+  end
+
+end
