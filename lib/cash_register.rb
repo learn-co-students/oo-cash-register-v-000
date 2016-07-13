74c8522508fec31a790f2a82e1a9a8ee60934521
Onyekachi <onye@smmaven.com>
2016-07-12 23:35:25 -0700

    OO Cash Register

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+class CashRegister
+	@@prices = []
+	@@items = []
 
+	attr_accessor :total, :discount
+
+	def initialize(discount = 0)
+		@total = 0
+		@discount = discount
+		@@items.clear
+	end
+
+	def add_item(title, price, quantity = 1)
+		@total += price*quantity
+		@@prices << price
+		@@items.concat([title]*quantity)
+	end
+
+	def apply_discount
+		@total = @total * (100 - @discount)/100
+		@discount == 0 ? "There is no discount to apply." :  "After the discount, the total comes to $800."
+	end
+
+	def items
+		@@items
+	end
+
+	def void_last_transaction
+		@total = @total - @@prices.pop
+	end
+end
