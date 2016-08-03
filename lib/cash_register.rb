bb3a7c8ebe0564d87ef143cc9ac52fd7727bc466
William Jones <enquiries@forestfed.com>
2016-08-03 10:37:07 +0000

    write OO methods

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,38 @@
+class CashRegister
 
+  attr_accessor :total, :discount, :items, :last_transaction_amount
+
+  def initialize(discount=0)
+    @total = 0
+    @last_transaction_amount = 0
+    @discount = discount
+    @items = []
+  end
+
+  def total
+    @total
+  end
+
+  def add_item(title, price, quantity=1)
+    @total = @total + (price * quantity)
+    @last_transaction_amount = price * quantity
+    quantity.times do
+      @items << title
+    end
+  end
+
+  def apply_discount
+    if @discount == 0
+      string = "There is no discount to apply."
+    else
+      discount_percentage = 100 - @discount
+      @total = (@total * discount_percentage) / 100
+      string = "After the discount, the total comes to $#{@total}."
+    end
+  end
+
+  def void_last_transaction
+    @total = @total - @last_transaction_amount
+  end
+
+end
