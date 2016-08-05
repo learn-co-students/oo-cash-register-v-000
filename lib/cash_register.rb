ee99eed45f655dbb0ea3a95fc4a9b8f67d55749e
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-08-05 22:40:45 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
+require 'pry'
 
+class CashRegister
+
+  attr_accessor :total
+
+
+  def initialize(discount=nil)
+    @discount = discount
+    @total = 0
+  end
+
+  def discount
+    @discount
+  end
+
+  def add_item(item, price, quantity=0)
+    quantity > 0 ? @total += price * quantity : @total += price
+  end
+
+  def apply_discount
+    self.total = self.total - self.discount
+  end
+
+
+end
