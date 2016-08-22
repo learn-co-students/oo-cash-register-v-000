f67a58be280b562090dad11e59820fd67225887d
Malind Nikulski <malind.nikulski@gmail.com>
2016-08-22 16:30:40 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,12 +14,20 @@ class CashRegister
   end
 
   def apply_discount
-    @total = subtotal
+    @total = total
     if @discount > 0
       @total -= ((@total * @discount) / 100)
       "After the discount, the total comes to $#{@total}."
     else
       "There is no discount to apply."
     end
+  end
+
+  def items
+    @items
+  end
+
+  def void_last_transaction
+    @total -= @last_transaction
   end
 end
