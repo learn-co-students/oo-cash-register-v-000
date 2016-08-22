438685feb58edfad778d4d2abaae5fbdeb8e9eab
Malind Nikulski <malind.nikulski@gmail.com>
2016-08-22 16:17:46 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
+class CashRegister
+  attr_accessor :total, :discount, :last_transaction
 
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity
+    quantity.times {@items << title}
+    @last_transaction = price * quantity
+  end
+end
