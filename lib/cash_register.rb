19b4fd97156a8dd1a0a4a67b96a14b6e97ba92a3
mo6709 <mo6709@gmail.com>
2016-09-13 17:29:10 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,36 +1,33 @@
 class CashRegister
 
-	attr_accessor :total, :discount, :item_array
+	attr_accessor :total, :discount, :items, :last_transaction
 
 	def initialize(discount=0)
 		@discount = discount
 		@total = 0
-		@item_array = []
+		@items = []
 	end
     
     def add_item(title, price, quantity=1) 
-        @item_array << [Array.new(quantity, title), price].flatten
-        @total += (price * quantity) 
+    	@total += (price * quantity)
+    	quantity.times  do
+    		@items<< title
+    	end
+        self.last_transaction = price * quantity
     end
 
     def apply_discount
-    	if @discount > 0
-    	    disc = (100 - @discount).to_f / 100
-    	    @total *= disc
-    	    "After the discount, the total comes to $#{@total.to_i}."
+
+    	if discount  != 0
+		    self.total = (total * ((100 - discount.to_f) / 100)).to_i
+		    "After the discount, the total comes to $#{self.total}."
     	else
     		"There is no discount to apply."
     	end 
     end
-    
-    def items
-    	@item_array.flatten.find_all{ |element| element.is_a?(String)}
-    end
-    
+       
     def void_last_transaction
-    	@total -= @item_array[-1][-1]
+    	@total -= @last_transaction
     end
 
 end
-
-#fullstacck@flatiron.com
