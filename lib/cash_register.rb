bf63dbfdd4170fd13a36d07eda7e4362d0fe1d8d
vanita eckert <vanita112003@gmail.com>
2016-09-16 17:03:50 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,22 @@
+require "pry"
+class CashRegister
 
+attr_reader :discount
+attr_accessor :total, :add_item
+
+  def initialize(discount=20)
+    @total = 0
+    @discount = discount
+  end
+
+
+def total
+@current_total = 100
+end
+
+def add_item(title, price)
+@current_total = @current_total + price
+# binding.pry
+end
+
+end
