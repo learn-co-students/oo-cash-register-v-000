cc646ba191c2f5a731660b0e2bb593e9a6bed503
vanita eckert <vanita112003@gmail.com>
2016-09-19 19:24:59 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,33 +2,33 @@ require "pry"
 class CashRegister
 
 attr_reader :discount
-attr_accessor :total
+attr_accessor :total, :items
 attr_writer :apply_discount
 
 
   def initialize(discount=0)
     @total = 0
     @discount = discount
+    @items = []
   end
 
   def add_item(title, price, quantity = 1)
-  @total = @total + (price*quantity)
+  self.total = self.total + price*quantity
+  # binding.pry
+  
+ quantity.times { @items << title }
    end
 
 
   def apply_discount
     if discount != 0
-      @discount = @discount.to_f/100 * @total
-      @total = @total - @discount
-        "After the discount, the total comes to $#{@total.to_i}."
+      @discount = @discount.to_f/100 * self.total
+      self.total = self.total - @discount
+        "After the discount, the total comes to $#{self.total.to_i}."
     else
       "There is no discount to apply."
     end
   end
 
-  def items
-    [].push(title)
-    # binding.pry
-  end
 
 end
