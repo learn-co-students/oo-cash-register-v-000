2d7b2e07ffdfc432e216c03cc0bc08dd80b79793
Aniqa Elahi <aniqaelahi@yahoo.com>
2016-11-17 17:47:59 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,19 @@
 
+class CashRegister
+  attr_accessor :discount, :total
+  @@transactions = []
+
+ def initialize(discount=0)
+   @total = 0
+   @discount = discount
+ end
+def add_item(item,price,quantity= 1)
+@total += price * quantity
+@@transactions << @total
+end
+
+def apply_discount
+    @total -= self.discount
+  return "After the discount, the total comes to $#{@total}."
+end
+end
