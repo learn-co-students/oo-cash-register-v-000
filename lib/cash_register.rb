5b202ae3214a64bda32aa98067085faeafafe6af
Tayari Appiah <cjhhh.ta@gmail.com>
2016-11-23 19:39:24 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,39 @@
 
+class CashRegister
+
+attr_accessor :total, :discount, :add_item
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @add_item = []
+  end
+
+
+
+  def add_item(title, price, optional = 1)
+    self.total += price * optional
+    self.add_item << title
+  end
+
+
+  def apply_discount(item, discount = 0)
+    if condition
+      self.discount = discount
+    else
+     "There is no discount to apply."
+    end
+  end
+
+
+   def items
+     self.add_item
+   end
+
+
+   def void_last_transaction
+    self.total -= self.total
+   end
+
+
+end
