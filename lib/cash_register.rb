43dcabf758967e54f6cd35acbfd8e2b0044aced6
Daniel Warren <warrend555@gmail.com>
2016-12-01 23:58:21 -0500

    finishes oo-cash-register

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,33 @@
+class CashRegister
+  attr_accessor :total, :discount, :items, :last
+
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+    @items = []
+    @last = 0
+  end
+
+  def add_item(item, price, quantity=1)
+    @total += (price * quantity)
+      quantity.times do
+        @items << item
+        @last = (price * quantity).to_f
+      end
+      @items
+  end
+
+  def apply_discount
+    if discount == 0
+      "There is no discount to apply."
+    else
+      self.total = total - (total * (discount / 100.to_f)).to_i
+      "After the discount, the total comes to $#{@total}."
+    end
+  end
+
+  def void_last_transaction
+    self.total -= @last
+  end
+end
 
