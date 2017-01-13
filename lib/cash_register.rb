9d018d4054fe4e72a3bc0c3e9e94c4570152304d
ryanpmcmahon <ryan@mcmahon.work>
2017-01-12 22:22:15 -0500

    completed cash register class

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,34 @@
 
+class CashRegister
+  attr_accessor :total, :discount, :items, :receipt
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+    @receipt = []
+  end
+
+  def add_item(item, price, quantity = 1)
+    cost = price*quantity
+    self.total += cost
+    quantity.times do
+      self.items << item
+    end
+    self.receipt << cost
+  end
+
+  def apply_discount
+    if self.discount > 0
+      self.total *= (1-self.discount*0.01)
+      "After the discount, the total comes to $#{total.floor}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def void_last_transaction
+    self.total -= self.receipt.pop
+  end
+
+end
