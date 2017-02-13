fdc926f1d941d214ca01ba7cba18017e78766777
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-02-13 16:40:08 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,57 @@
+require "pry"
+class CashRegister
+  attr_accessor :total, :items, :discount
+  @last_item = ''
+  @last_price = 0
+  @discount_applied = false
 
+  def initialize(discount = 0)
+    @total = 0
+    @items = []
+    @discount = discount
+    @last_item = ''
+    @last_price = 0
+    @discount_applied = false
+  end
+
+  def add_item(item,price,quantity = 1)
+    quantity.times do
+      @items << item
+      @total += price
+      @last_item = item
+      @last_price = price
+    end
+    return item
+  end
+
+  def apply_discount
+    if @discount > 0 && !@discount_applied
+    @total *= ((100.0-@discount)/100.0)
+    @total = round_total(@total)
+    @discount_applied = true
+    # binding.pry
+    return "After the discount, the total comes to $#{@total}."
+    else
+      return "There is no discount to apply."
+    end
+  end
+
+  def total
+
+    round_total(@total)
+  end
+
+  def round_total(total)
+    if total == total.round(0)
+      total = total.round(0)
+    else
+      total = total.round(2)
+    end
+    total
+  end
+
+  def void_last_transaction
+    @total -= @last_price
+    @items.pop
+  end
+end
