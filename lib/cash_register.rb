b5e29b494ee181f5f1f5b9b1ae31fa6ead86b479
charlie lee <ctlee9096@gmail.com>
2017-02-16 03:19:08 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,11 @@
+require 'pry'
+class CashRegister
+  attr_accessor :total, :discount#read&write method of variable total
+  def initialize
+    @total, @discount = 0, 20
+  end
 
+  def cash_register_with_discount
+  end
+
+end
