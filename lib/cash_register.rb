b95c60899fa84005bb578808e86011bc62a6b6dc
Isis Tejeda <isis@isistejeda.com>
2017-03-08 12:08:05 -0600

    before rubocop

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,34 @@
 
+class CashRegister
+  attr_accessor :total, :discount, :items
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+
+  end
+
+  def add_item(title, price, quantity = 1)
+    @last_price = price
+    @last_quantity = quantity
+    @total += price * quantity
+    quantity.times do @items << title  end
+  end
+
+  def apply_discount
+    if discount == 0
+      return "There is no discount to apply."
+    else
+      @total = total - (total * discount / 100)
+      return "After the discount, the total comes to $#{total}."
+    end
+  end
+
+  def void_last_transaction
+    @total -= (@last_price * @last_quantity)
+    @items.pop(@last_quantity)
+  end
+
+
+end
