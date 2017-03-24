ff8bdaa97550fff7854016b395b8e79a660d68b9
thesilentc <thesilentc@me.com>
2017-03-24 15:05:35 -0700

    refactored #apply_discount

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -23,14 +23,13 @@ class CashRegister
   end
 
   def apply_discount
-    if @discount > 0
-      @to_take_off = (price * discount)/100
-      @total -= @to_take_off
-      return "After the discount, the total comes to $#{total}."
-    else
-      return "There is no discount to apply."
+      if @discount == 0
+        "There is no discount to apply."
+      else
+        @total -= (@total*@discount/100)
+        "After the discount, the total comes to $#{total}."
+      end
     end
-  end
 
 
   def void_last_transaction
