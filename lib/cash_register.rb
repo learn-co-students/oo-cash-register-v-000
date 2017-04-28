a130e1edf43ebb3c4bae192a6d2e376f09ffe146
hyoyou <heatherhyou@gmail.com>
2017-04-28 18:11:47 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,35 @@
+require 'pry'
 
+class CashRegister
+  attr_accessor :total, :discount
+
+  def initialize(discount = nil)
+    @total = 0
+    @discount = 20
+  end
+
+  def total
+    @total
+  end
+
+  def add_item(title, price)
+    total + price
+    #increases the total
+    #accepts and optional quantity
+    #doesn't forget about the previous total
+  end
+
+  def apply_discount
+
+  end
+
+  def items
+
+  end
+
+  def void_last_transaction
+
+  end
+
+
+end
