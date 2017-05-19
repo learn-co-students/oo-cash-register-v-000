53365508df2d4bb12d241667594d35b835f4215e
Danielle Croxton <danikrist@gmail.com>
2017-05-19 22:27:20 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
+class CashRegister
+  attr_accessor :item, :quantity, :price, :discount
 
+def initialize(discount)
+  @discount = discount
+  @total = 0
+end
+
+def total
+
+end
+
+def add_item
+
+end
+
+def apply_discount
+
+end
+
+def items
+
+end
+
+def void_last_transaction
+
+end
+
+end
