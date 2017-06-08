8d15fc46a9960a40ef1f23ecc8bb3c125e7d69bd
Jack Xu <jackexyou@gmail.com>
2017-06-08 15:44:13 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,13 @@
 
+class CashRegister
+  attr_accessor :total :discount
+
+  def initialize(discount=0)
+    @total = 0
+    @discount = 0
+  end
+
+  def add_item(item)
+    @total += item
+  end
+end
