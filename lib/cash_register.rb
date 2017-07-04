844234fb679d8fda449c73f0fbed1d24384e1aec
Sydney Chun <schun08@gmail.com>
2017-07-04 14:31:42 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,46 @@
-require 'pry'
+# first attempt 6/23/17
+# require 'pry'
+# class CashRegister
+#   attr_accessor :total, :discount, :items, :last_transaction_amount
+#
+#   def initialize(discount=0)
+#     @total = 0
+#     @discount = discount
+#     @items = []
+#   end
+#
+#   def add_item(title,price,quantity=1)
+#     self.total += price*quantity
+#     #use #times here to loop the required number of times
+#     quantity.times do
+#       self.items << title
+#     end
+#     self.last_transaction_amount = price*quantity
+#   end
+#
+#   def apply_discount
+#     result = "There is no discount to apply."
+#     if self.discount != 0
+#       #why is it okay to drop "self" here?
+#         #is it because you're in an instance method?
+#       self.total -= (self.total * (self.discount/100.0)).to_i
+#       result = "After the discount, the total comes to $#{total}."
+#     end
+#     result
+#   end
+#
+#   def void_last_transaction
+#     self.total -= self.last_transaction_amount
+#     #how would you remove the items from the last transaction from the items array?
+#       #would you need to store each element of #add_item into variables?
+#   end
+#
+# end
+
+# second attempt 7/4/17
 class CashRegister
-  attr_accessor :total, :discount, :items, :last_transaction_amount
+  attr_accessor :total, :items, :last_transaction
+  attr_reader :discount
 
   def initialize(discount=0)
     @total = 0
@@ -8,30 +48,22 @@ class CashRegister
     @items = []
   end
 
-  def add_item(title,price,quantity=1)
+  def add_item(title, price, quantity=1)
     self.total += price*quantity
-    #use #times here to loop the required number of times
-    quantity.times do
-      self.items << title
-    end
-    self.last_transaction_amount = price*quantity
+    self.last_transaction = price*quantity
+    quantity.times {self.items.push(title)}
   end
 
   def apply_discount
-    result = "There is no discount to apply."
     if self.discount != 0
-      #why is it okay to drop "self" here?
-        #is it because you're in an instance method?
       self.total -= (self.total * (self.discount/100.0)).to_i
-      result = "After the discount, the total comes to $#{total}."
+      "After the discount, the total comes to $#{self.total}."
+    else
+      "There is no discount to apply."
     end
-    result
   end
 
   def void_last_transaction
-    self.total -= self.last_transaction_amount
-    #how would you remove the items from the last transaction from the items array?
-      #would you need to store each element of #add_item into variables?
+    self.total -= self.last_transaction
   end
-
 end
