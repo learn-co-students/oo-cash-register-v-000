7203d7797e82d4fb42099e50fe1e9f998b1e3c81
Richard Steinbach <steinbach.dick@gmail.com>
2017-07-24 07:56:15 -0700

    OO cash register

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,44 @@
+class CashRegister
+	require 'pry'
+	attr_accessor :total, :discount, :quantity, :items, :price
+	#@@total = 0
+	
 
+
+	def initialize(discount = 0)
+		@discount = discount
+		@total = 0
+		@items = []
+	end
+
+	def add_item(item, price, quantity = 1)
+		@quantity = quantity
+		#@@total += @total
+		@price = price
+		@total += (price * quantity)
+			while quantity > 0
+				quantity -= 1
+				@items << item
+			end
+	end
+
+	def apply_discount
+	
+		if @discount > 0 
+			@total = ((((100 - discount).to_f) / 100) * @total).to_i
+			"After the discount, the total comes to $#{@total}."
+		else
+			"There is no discount to apply."
+		end
+	end
+
+	def items
+		@items
+	end
+
+	def void_last_transaction
+		#binding.pry
+		@total = @total - @price
+	end
+
+end
