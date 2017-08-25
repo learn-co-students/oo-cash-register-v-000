ace847bd4b7b56f60a4d01693704b4c093f3ec27
Susan Fowler <infopro94@gmail.com>
2017-08-25 21:58:34 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,33 @@
+class CashRegister #KISS, Susan!
 
+  attr_accessor :total, :discount, :items
+  # attr_accessor :items, :discount, :total, :last_transaction
+
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+  end
+
+  def total
+    @total  #equals sum of all items
+  end
+
+  def add_item(title, price, quantity=0)
+      #increases total
+    #accepts optional quantity
+    #include previous total
+  end
+
+  def apply_discount
+
+  end
+
+  def items
+    #returns array of all items added
+  end
+
+  def void_last_transaction
+    #subtracts the last transaction from the total
+  end
+
+end
