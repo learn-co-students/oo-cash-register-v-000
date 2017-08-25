624233b12ac452408b78e737a0168cbaab16d01b
Susan Fowler <infopro94@gmail.com>
2017-08-25 23:04:33 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister #KISS, Susan!
 
-  attr_accessor :total, :discount, :item
+  attr_accessor :total, :discount, :item, :price, :discount_price
   # attr_accessor :last_transaction
 
   def initialize(discount=0)
@@ -12,20 +12,23 @@ class CashRegister #KISS, Susan!
     @total  #equals sum of all items
   end
 
-  def add_item(title, price, quantity=0)
+  def add_item(title, price, quantity=1)
     item = {:title=>price}
-    price*quantity = subtotal
-    #increases total
-    #accepts optional quantity
-    #include previous total
+    self.total+=price*quantity
   end
 
   def apply_discount
-
-  end
+    @discount == (@discount/100.0)
+    @discount_price == (@total * @discount)
+    if self.discount == 0
+      "There is no discount to apply."
+    else
+      "After the discount, the total comes to #{discount_price}."
+     end
+   end
 
   def items
-    #returns array of all items added
+    self.items = []#returns array of all items added
   end
 
   def void_last_transaction
