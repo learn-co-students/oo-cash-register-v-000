117a60e9902011cff65996138f8be83e15d19f0c
Susan Fowler <infopro94@gmail.com>
2017-08-26 21:44:32 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,8 @@
+require 'pry'
+
 class CashRegister #KISS, Susan!
 
-  attr_accessor :total, :discount, :item, :price, :discount_price
-  # attr_accessor :last_transaction
+  attr_accessor :total, :discount, :items, :price, :last_transaction
 
   def initialize(discount=0)
     @total = 0
@@ -13,26 +14,27 @@ class CashRegister #KISS, Susan!
   end
 
   def add_item(title, price, quantity=1)
-    item = {:title=>price}
+    @items = {:title=>price}
     self.total+=price*quantity
   end
 
   def apply_discount
-    @discount == (@discount/100.0)
-    @discount_price == (@total * @discount)
-    if self.discount == 0
-      "There is no discount to apply."
-    else
-      "After the discount, the total comes to #{discount_price}."
-     end
+    @total = @total - (@total * @discount.to_f/100)
+     if self.discount == 0
+       "There is no discount to apply."
+     else
+       "After the discount, the total comes to $#{total.to_i}."
+      end
    end
 
-  def items
-    self.items = []#returns array of all items added
+  def items #returns array of all items added 
+    #Iterate over the items hash to collect the keys
+    # binding.pry
+    @items
   end
 
   def void_last_transaction
-    #subtracts the last transaction from the total
+    @total.to_i -= @last_transaction #subtracts the last transaction from the total
   end
 
 end
