3cc2410f791a73597acd5cd941987c6c96acc1fa
Gustavo Villagrana <gusvilla303@gmail.com>
2017-09-06 04:33:03 +0000

    Implement #items method

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,21 +2,26 @@ require 'pry'
 
 class CashRegister
 
-  attr_accessor :total, :discount
+  attr_accessor :total, :discount, :items
 
   def initialize(discount=0)
     @total    = 0
     @discount = discount
+    @items    = []
   end
 
   def add_item(title, price, opt_quantity=1)
+    @items << title.split * opt_quantity
+    self.items = @items.flatten
     self.total += price * opt_quantity
   end
 
   def apply_discount
-    self.total -= self.discount * 10
-    puts "After the discount, the total comes to $#{@total}."
+    if self.discount > 0
+      self.total -= self.discount * 10
+      "After the discount, the total comes to $#{@total}."
+    else
+      "There is no discount to apply."
+    end
   end
-
-
 end
