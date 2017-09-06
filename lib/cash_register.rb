19c248d82e41902e6071d24663928d9308930535
Gustavo Villagrana <gusvilla303@gmail.com>
2017-09-06 02:47:32 +0000

    Implement #apply_discount method

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,22 @@
+require 'pry'
 
+class CashRegister
+
+  attr_accessor :total, :discount
+
+  def initialize(discount=0)
+    @total    = 0
+    @discount = discount
+  end
+
+  def add_item(title, price, opt_quantity=1)
+    self.total += price * opt_quantity
+  end
+
+  def apply_discount
+    self.total -= self.discount * 10
+    puts "After the discount, the total comes to $#{@total}."
+  end
+
+
+end
