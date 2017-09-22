93443cd2512cc89de25dc9a17275e4838ae5480e
Ruth Mesfun <ruth@flatironschool.com>
2017-09-22 12:14:23 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,39 @@
 
+class CashRegister
+  attr_accessor :discount, :total, :items, :price
+  # attr_reader :total <-- Q: Not sure why this isnt working.
+  def initialize(discount = 0)
+    @items = Array.new
+    @discount = discount
+    @total = 0
+  end
+
+  def add_item(item, price, qty = 1)
+    self.price = price * qty
+    self.total += self.price
+    while qty > 0
+      self.items << item
+      qty-=1
+    end
+    self.total
+  end
+
+  def apply_discount
+    if self.discount > 0
+      percentage_of_total = 1 - (self.discount * 0.01)
+      self.total *= percentage_of_total
+      self.total
+      "After the discount, the total comes to $#{self.total.to_i}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def void_last_transaction
+    self.total -= self.price
+    self.items.clear
+    self.total
+
+  end
+
+end
