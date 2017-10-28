2e1d6556d65b82ab1bcd65dc6d8f4193cff6814f
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-28 00:56:59 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,18 @@
+class CashRegister
 
+  attr_accessor :total, :discount
+
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item(title,price,quantity=1)
+
+  end
+
+end
+
+
+cash_register = CashRegister.new.add_item("eggs", 0.98)
+puts cash_register.total
