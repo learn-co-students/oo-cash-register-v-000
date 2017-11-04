bfdfe03d00534b261f2ac31c5b31718ba8df9aba
Christopher DiStasio <github email address>
2017-11-04 17:57:10 -0500

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
-
+class CashRegister
+    
+        attr_accessor :discount, :total
+    
+        def initialize(discount = 0)
+            
+            @total = 0
+    
+            @discount = discount
+    
+        end
+    
+        def add_item(title, price)
+        end
+      
+end
