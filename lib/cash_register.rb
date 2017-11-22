860697b08088fbcf8abb7241c82b0632dd1d2c80
charityyoungblood <youngblood.charity@gmail.com>
2017-11-22 10:19:42 -0800

    added @total and in initialize method with comparison operator

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,11 +2,11 @@ require 'pry'
 
 class CashRegister 
 # set @total instance variable 
-  attr_accessor :total, :discount
-  @total
+  attr_accessor :total 
+  @total = 0
 # initialize @total instance variable to 0 
-  def new 
-    @total = 0 
+  def initialize
+    @total == 0
   end 
   #def initialize
     
--- spec/cash_register_spec.rb
+++ spec/cash_register_spec.rb
@@ -4,7 +4,7 @@ describe 'CashRegister' do
   let(:cash_register) { CashRegister.new }
   let(:cash_register_with_discount) { CashRegister.new(20) }
 
-  describe '::new' do binding.pry
+  describe '::new' do 
     it 'sets an instance variable @total on initialization to zero' do
       expect(cash_register.instance_variable_get(:@total)).to eq(0)
     end
