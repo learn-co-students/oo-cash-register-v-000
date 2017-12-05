aed51d4309cc9a897d8ac6a5e9c37da1794572e6
Adjoa Darien <adjoa.darien@gmail.com>
2017-12-05 02:23:27 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
+class CashRegister
+  attr_accessor :total, :discount, :title, :price
 
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity
+  end
+
+  def apply_discount
+    
+  end
+end
