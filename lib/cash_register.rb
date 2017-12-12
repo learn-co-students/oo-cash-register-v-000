4b84f3478c8e967dbbc6643da4bf2a23ff9d3bcb
github username <github email address>
2017-12-12 09:33:43 -0500

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
+require 'pry'
 
+class CashRegister
+
+  attr_accessor :total, :discount, :items, :prices, :quantity, :title, :last_transaction_amount
+
+  def initialize(discount=0)#employee discount
+    @total = 0#sets total to zero
+    @discount = discount
+    @items = []#array of items
+  end
+
+  def add_item(title, price, quantity = 1)
+    @last_transaction_amount = price*quantity
+    @total += @last_transaction_amount
+    quantity.times { @items << title }
+  end
+
+  def apply_discount
+    @total*@discount
+  end
+
+
+end
