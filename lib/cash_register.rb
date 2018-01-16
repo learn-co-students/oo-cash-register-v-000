bf39b438aaa8d7b29c9d22d3adf838a641b7d2ae
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-16 17:38:08 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,38 @@
+require 'pry'
+class CashRegister
 
+  attr_accessor :total, :discount
+  @@items = []
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item(title, price, quantity = 1)
+    @title = title
+    @price = price
+    @quantity = quantity
+    @total += (@price * @quantity)
+    i = 0
+    while i < @quantity-1
+      @@items << @title
+      i += 1
+    end
+  end
+
+  def apply_discount
+    if @discount != 0
+      discount_percent = @discount/100.to_f
+      @total = @total * (1-discount_percent)
+      "After the discount, the total comes to $#{@total.to_i}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def items
+    @@items
+  end
+
+end
