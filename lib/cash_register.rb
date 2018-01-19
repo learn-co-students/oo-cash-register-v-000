6e2fb96b2dcf23fe7ba8c1375f3f5fa8c0d59371
jljardon <github email address>
2018-01-19 00:17:01 -0500

    add functionality to CashRegister

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
+require 'pry'
+class CashRegister
+   attr_accessor :total, :discount, :items, :last_total, :last_quantity
 
+   def initialize(discount = 0)
+      @total = 0
+      @discount = discount
+      @items = []
+   end
+
+   def add_item(title, price, quantity = 1)
+      @last_total = @total
+      @last_quantity = quantity
+      @total += price * quantity
+
+      quantity.times do
+         @items.push(title)
+      end
+   end
+
+   def apply_discount
+      if @discount == 0
+         'There is no discount to apply.'
+      else
+         @total -= @total.to_f * (@discount.to_f / 100.0)
+         "After the discount, the total comes to $#{@total.to_i}."
+      end
+   end
+
+   def void_last_transaction
+      @total = @last_total
+
+      @last_quantity.times do
+         @items.pop
+      end
+   end
+end
