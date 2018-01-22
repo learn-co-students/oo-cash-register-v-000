31da4c805b650355627331f6e5863977fbc09231
mnimmz <morgannimmons@gmail.com>
2018-01-21 19:53:24 -0500

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,10 +8,6 @@ class CashRegister
     @items = []
   end
 
-  def add
-    self.class.items << self
-  end
-
   def add_item(title, price, quantity=1)
     self.total += price * quantity
     quantity.times do
