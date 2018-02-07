968b0db2f0ed8b499e48b906034daed1fccbbc1e
Kriti Rai <kriti.rai@gmail.com>
2018-02-07 19:17:39 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
 
+class CashRegister
+  attr_accessor :total, :discount, :price, :qty
+
+
+  def initialize(total = 0, discount = 0)
+   @total = total
+
+   @discount = discount
+ end
+
+ def total
+   @total
+ end
+
+ def add_item(item, price, qty = 1)
+   @price = price
+   @qty = qty
+   @total += @price * @qty
+ end
+
+ def apply_discount
+   discount = (@discount.to_f/100)
+   total_discount = (@total.to_f * discount)
+   @total = @total.to_f - total_discount
+  #  puts "After the discount, the total comes to #{@total}."
+ end
+
+end
