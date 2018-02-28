e65f4d1d2d67f8a22f344d54ac81110e3a4b9163
emi kojima <emi8emi@gmail.com>
2018-02-28 22:34:27 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,53 @@
+require'pry'
+class CashRegister
+  attr_accessor :total, :discount, :last_transaction 
 
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount 
+    @cart = []
+  end 
+  
+  def total 
+    @total 
+  end 
+  
+  def add_item(title, price, quantity = 1)
+    @last_transaction = (quantity*price) 
+    @total += (quantity*price) 
+   
+    items = {}
+    items[:title] = title 
+    items[:price] = price 
+    items[:quantity] = quantity
+    @cart << items 
+   end 
+     
+  def apply_discount
+    if @discount > 0
+    @total -= (@total.to_f*discount.to_f/100)
+    "After the discount, the total comes to $#{@total.to_i}."
+    else
+    "There is no discount to apply."
+    end
+  end 
+  
+  def items
+    item_names = []
+    @cart.each do |item_info|
+      #test expects product name * quantity...
+      for qty in 1..item_info[:quantity] 
+        item_names << item_info[:title]
+      end 
+    end 
+    item_names
+  end 
+
+   #@cart.map{|i| i[:title]* i[:quantity]}
+   #binding.pry 
+  
+  def void_last_transaction
+   @total -= @last_transaction
+  end
+end
