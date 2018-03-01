423d4ad5088f88f3e20b155fa9bf9cfa4d30898c
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-03-01 02:59:58 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,17 @@
+require 'pry'
 
+class CashRegister
+
+  def initialize(discount = 20)
+    @total = 0
+    def self.discount
+      discount = 20
+    end
+  end
+
+  def total=
+    @total
+
+  end
+
+end
