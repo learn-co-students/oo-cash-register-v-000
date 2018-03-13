09a9259cc3da099cdab1265a9345372302e4edc1
Katie Cassell <kcassell944@gmail.com>
2018-03-13 20:40:09 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
+require 'pry'
+class CashRegister
+attr_accessor :total, :discount, :quantity
 
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item(title, price, quantity = 1)
+    subtotal = price * quantity
+    self.total += subtotal
+  end
+
+  def apply_discount
+  end
+
+  def items
+  end
+
+  def void_last_transaction
+  end
+
+end
