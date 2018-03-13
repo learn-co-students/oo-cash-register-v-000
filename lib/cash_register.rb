c7a4e33100536385ce17f6b018ee8370f701753a
cam <camsay@gmail.com>
2018-03-13 15:10:31 -0700

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,48 @@
+class CashRegister
 
+  attr_accessor :total, :discount, :running_total
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+    @running_total = []
+
+  end
+
+  def total
+    @total
+  end
+
+  def add_item(item, price, quantity = 1)
+
+    new_items = Array.new(quantity, item)
+    (@items << new_items).flatten!
+    each_price = Array.new(quantity, price)
+    (@running_total << each_price).flatten!
+
+
+    @total = quantity * price + @total
+
+
+  end
+
+  def apply_discount
+    if @discount == 0
+      "There is no discount to apply."
+    elsif
+      off = @discount.to_f / 100 * @total
+      @total = (@total - off).to_i
+        "After the discount, the total comes to $#{@total}."
+    end
+  end
+
+  def void_last_transaction
+    @total = @total - @running_total.pop
+  end
+
+  def items
+    @items
+  end
+
+end
