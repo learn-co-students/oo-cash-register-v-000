3ff348b1351a8a7d6fcd7a8b7b442427d0556e04
Kitty Davies <kittyfdavies@gmail.com>
2018-03-29 02:00:26 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,28 @@
+class CashRegister
+  attr_accessor :total, :items
 
+  def initialize(apply_discount = 0)
+    @total = 0 + apply_discount
+  end
+
+  def total
+    @total
+  end
+
+  def add_item(title, price, quantity = 1)
+    @total += price*quantity
+  end
+
+  def apply_discount
+
+  end
+
+  def items
+
+  end
+
+  def void_last_transaction
+    @total = self.total - self.total
+  end
+
+end
