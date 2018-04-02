b7460c15314d56a37270c2916b8118b8e5624e63
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-04-02 12:10:44 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,18 +10,18 @@ class CashRegister
     @all_items = []
   end
 
-  def add_item(title, price, quantity = 1)
-    @total = total + (price * quantity)
+  def add_item(item, price, quantity = 1)
+    self.total += (price * quantity)
     quantity.times do
-      @all_items << title
+      @all_items << item
     end
-    @last_transaction = price * quantity
+    self.last_transaction = price * quantity
   end
 
   def apply_discount
     if discount > 0.to_f
-      @total = total - (total * discount/100.to_f)
-      "After the discount, the total comes to $#{@total.to_i}."
+      self.total -= (total * discount/100.to_f)
+      "After the discount, the total comes to $#{self.total.to_i}."
     else
       "There is no discount to apply."
     end
@@ -32,7 +32,7 @@ class CashRegister
   end
 
   def void_last_transaction
-    @total = total - last_transaction
+    self.total -= last_transaction
   end
 
 end
