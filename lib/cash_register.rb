049deae25fe5005edfc3e037eae4bd6ca7127ea0
Erica Murdock-Waters <ericahpa@gmail.com>
2018-04-21 15:12:40 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,8 +6,8 @@ class CashRegister
   def initialize(discount = 0)      # optionally takes an employee discount on initialization
     @total = 0                      #sets an instance variable @total on initialization to zero
     @discount = discount
-    @@items = []                    # stores all title attributes for individual instance of cash_register
-    @@transactions = []             # stores all transactions for individual instance of cash_register
+    @items = []                    # stores all title attributes for individual instance of cash_register
+    @transactions = []             # stores all transactions for individual instance of cash_register
   end
 
   def add_item(title, price, quantity = 0)
@@ -16,13 +16,13 @@ class CashRegister
     @quantity = quantity              # also accepts an optional quantity
 
     if quantity > 0
-      @@items.fill(@title, @@items.size, @quantity)
+      @items.fill(@title, @items.size, @quantity)
       self.total += (@price * @quantity)
     else                            # doesn't forget about the previous total
-      @@items << self.title
+      @items << self.title
       self.total += (@price)        # increases the total by using '+='
     end
-    @@transactions << self
+    @transactions << self
   end
 
   def total=(total)               # returns the current total
@@ -39,7 +39,7 @@ class CashRegister
   end
 
   def items                       # returns an array containing all items that have been added
-    @@items                       # and saved in #add_item method
+    @items                       # and saved in #add_item method
   end
 
   def void_last_transaction       # subtracts the last transaction from the total
