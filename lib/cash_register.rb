fb2ce7f126d787f3c75fc000ce1640eabe00ed03
Joseph Parker <jparker010190@gmail.com>
2018-04-25 23:49:07 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
-
+class CashRegister
+  attr_accessor :discount, :item, :last_transaction, :total
+  def initialize(discount = 0)
+    @items = []
+    @discount = discount
+    @total = 0
+  end
+  def add_item(cost, name , number = 1)
+    self.total = cost * number
+    @items << name
+  end
+  def add_item2(cost,name,number = 1)
+    self.total = cost * number
+  end
+  def apply_discount
+  end
+  def items
+  end
+  def void_last_transaction
+  end
+end
