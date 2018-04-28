c1fa535ebe9de31cb84725f62fa201e248c15275
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-28 21:00:32 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,22 @@
+class CashRegister
 
+attr_accessor :total, :discount
+
+def initialize(discount = 0)
+  @discount = discount
+  @total = 0
+end
+
+def add_item
+end
+
+def apply_discount
+end
+
+def items
+end
+
+def void_last_transaction
+end
+
+end
