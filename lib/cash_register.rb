f1b3669835b210b7b3b874f966341cbc46a06c19
Emily Welsch <emwelsch@gmail.com>
2018-05-26 21:16:29 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,43 @@
+require 'pry'
+# binding.pry
+# learn/rspec --fail-fast
 
+class CashRegister
+attr_accessor :total, :discount, :price, :items
+
+def initialize(discount = 0)
+  @total = 0
+  @discount = discount
+  @items = []
+end
+
+def apply_discount
+  if @discount > 0
+    @discount_amount = (price * discount)/100
+    @total -= @discount_amount
+    return "After the discount, the total comes to $#{total}."
+  else
+    return "There is no discount to apply."
+  end
+end
+
+def add_item(item, price, quantity = 1)
+  @price = price
+  @total += price * quantity
+  if quantity > 1
+    counter = 0
+    while counter < quantity
+      @items << items
+      counter += 1
+    end
+  else
+    @items << item
+  end
+end
+
+def void_last_transaction
+     @total -= @price
+end
+
+
+end
