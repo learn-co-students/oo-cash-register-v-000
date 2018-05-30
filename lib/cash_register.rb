c017cae6b60cad9028d691dfae09bce31c35d4b5
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-30 11:41:17 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,15 +1,19 @@
 class CashRegister
 
-attr_accessor :total, :discount, :items
+attr_accessor :total, :discount, :items, :prices
 
   def initialize(discount = nil)
     @total = 0
     @discount = discount
     @items = []
+    @prices = []
   end
 
   def add_item(item, price, quantity = 1)
-    quantity.times(self.items << item)
+    quantity.times do
+    self.items << item
+    self.prices << price
+    end
     self.total = self.total + price * quantity
   end
 
@@ -23,5 +27,9 @@ attr_accessor :total, :discount, :items
     "After the discount, the total comes to $#{self.total}."
   end
 
-  
+  def void_last_transaction
+    self.total = self.total - self.prices.last
+  end
+
+
 end
