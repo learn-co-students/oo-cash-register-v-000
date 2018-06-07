e004e2eb56ea04971fc4431e5a364a85b449a2a4
Aziz Kosber <a.kosber@gmail.com>
2018-06-07 13:45:05 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,4 +1,5 @@
 require 'pry'
+#ok look, this code works and the solution is pretty good. However the code can be more elegant. for example I never use self here.
 class CashRegister
   attr_accessor :total, :discount, :items, :order_list
 
@@ -12,20 +13,20 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     @order_list << { "item_name" => title, "item_price" => price, "quantity_ordered" => quantity }
     quantity.times { @items << title }
-    @total = @total + (price * quantity)
+    self.total += (price * quantity)
   end
 
   def apply_discount
     if @discount == nil
       "There is no discount to apply."
     else
-      @total = (@total * (1 - @discount/100.0)).to_i
-      "After the discount, the total comes to $#{@total}."
+      self.total = (total * (1 - discount/100.0)).to_i
+      "After the discount, the total comes to $#{self.total}."
     end
   end
 
   def void_last_transaction
-    @total = @total - (@order_list.last["item_price"])
-    @total
+    self.total = total - (@order_list.last["item_price"])
+
   end
 end
