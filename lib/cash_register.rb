49f0c5fb73daceabeaa3214cecf5b1baf3e67324
Aziz Kosber <a.kosber@gmail.com>
2018-06-07 13:17:38 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,13 +1,17 @@
 require 'pry'
 class CashRegister
-  attr_accessor :total, :discount
+  attr_accessor :total, :discount, :items, :order_list
 
   def initialize(discount = nil)
     @total = 0
     @discount = discount
+    @items = []
+    @order_list = []
   end
 
   def add_item(title, price, quantity = 1)
+    @order_list << { "item_name" => title, "item_price" => price, "quantity_ordered" => quantity }
+    quantity.times { @items << title }
     @total = @total + (price * quantity)
   end
 
@@ -18,5 +22,10 @@ class CashRegister
       @total = (@total * (1 - @discount/100.0)).to_i
       "After the discount, the total comes to $#{@total}."
     end
+  end
+
+  def void_last_transaction
+    @total = @total - (@order_list.last["item_price"])
+    @total
   end
 end
