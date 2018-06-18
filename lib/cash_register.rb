eebf057ddeeed98de675db0d84ecf36c63b9f8db
Anusha Konatala <anu.konatala@gmail.com>
2018-06-18 23:13:53 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,25 +1,23 @@
 class CashRegister
-attr_accessor :total, :discount, :item, :price
+attr_accessor :total, :discount, :item, :price, :items
 
-@@items = []
+#@@items = [] // this cant be a class variable as it items changes once you #instaniate an object;
 
 def initialize(discount = 0)
   @total = 0
   @discount = discount
-  @@items = []
+  @items = []
 end
 
 def add_item(item,price, quantity = 1)
  
-  until quantity < 1 do
+  while quantity > 0 do
      self.item = item
     self.price = price
-    @@items << item
+    @items << item
     @total += price
     quantity = quantity - 1
   end
-  
-  
 end  
 
 def apply_discount
@@ -32,12 +30,13 @@ def apply_discount
 end
 
 def items
-  @@items
+  @items
 end
 
 def void_last_transaction
   @total = @total - self.price
 end
+
 def total
   @total
 end
