989d47fcf77f049717cfbc8c8c15d9ca291a8fd5
Amit Singh <amit_nyc@live.com>
2018-07-01 16:59:17 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,17 +1,23 @@
-require 'pry'
+
 
 class CashRegister
 
 
 attr_accessor :total, :items, :discount, :last_transaction
 
-
+# when .new is called, the initialize method with be called and an instance variable
+#   total will be created; items with an empty array to store the items purchase ;
+#   a varibale called discount which will equal to discount passed in, if nothing is passed in discount then discount will be default to 0
 def initialize(discount = 0)
 @total = 0
 @items = []
 @discount = discount
 end
-
+#
+# the add_item method takes in 3 args, an item, price and quantity
+# total equals to price times quantity
+# item will then be pushed into the items variables
+# calling the class using self, last_transaction equals to price times quantity
 def add_item(item, price, quantity = 1)
   @total += price * quantity
   quantity.times do
@@ -19,6 +25,13 @@ def add_item(item, price, quantity = 1)
 end
 self.last_transaction = price * quantity
 end
+
+
+apply_discount method will check if a discount needs to be applied
+# if discount does not (!=) equal to 0 then
+#   total equals to 100 - discount to a floating number (2.1 etc..) divided by 100 times 100.0 and then change to Integer
+#   which puts out   "After the discount, the total comes to $#{@total}."
+#   if discount equals to 0 then method puts out   "There is no discount to apply."
 
 def apply_discount
   if discount != 0
@@ -28,6 +41,9 @@ def apply_discount
     "There is no discount to apply."
   end
 end
+
+# the void_last_transaction method called the total variable which would equal to
+# total - last_transaction
 
 def void_last_transaction
   self.total = self.total - self.last_transaction
