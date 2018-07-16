b3ed05c5ce442ce2ba3f8c5ccd32ab7fd88bc5c9
Pan Cheng <pchengeducation@hotmail.com>
2018-07-16 03:22:13 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
+class ClassRegister
 
+attr_accessor :discounts, :total
+
+def initialize(discount = nil)
+
+  @discount = discount
+  @total = 0
+end
+
+def add_item(item, price, amount = 1)
+  self.total = price * amount
+
+end
+
+def apply_discount
+if discount > 0 && total
+  self.total = (self.total * discount.to_f)/100)).to_i
+else
+  self.total
+end
+end
+
+end #end of the ClassRegister class
