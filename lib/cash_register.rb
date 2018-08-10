5f6a75d03e1ad28f10aaa39c3aad945dd8451762
Angie Kassab <anasab12@gmail.com>
2018-08-10 01:20:41 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
-
+class CashRegister
+  
+  attr_accessor :total, :discount, :item, :price, :quantity
+  
+  def initialize(employee_discount = 0)
+    @total = 0
+    @discount = employee_discount
+    
+  end
+  
+  def add_item(item, price, quantity = 1)
+    self.total += (price * quantity)
+  end
+  
+end
