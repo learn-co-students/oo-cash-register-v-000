1ad3d771ead620eedbf184e1a4595a4d321c4cb1
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-08-17 01:46:09 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
-
+class CashRegister
+  attr_accessor :total, :discount, :items
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+  
+  def self.total 
+    @total 
+  end
+  
+  def self.add_item(item, price, quantity=1)
+     self.total += price*quantity
+     @items << item
+     
+  end
+  
+  def self.apply_discount(discount)
+    x = discount.to_i
+    self.total *= x 
+  end
+  
+    
+    
+    
+  
+end
