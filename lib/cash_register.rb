95fd208e31e88bb21eb423fd5e34374764c46bbb
Rafael Lopez <lpz.raf@gmail.com>
2018-08-22 03:24:20 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,9 +12,8 @@ class CashRegister
   
    def add_item(title, price, quantity = 1)
     self.total = price * quantity
+    total
   end
 end
 
-# accepts a title and a price and increases the total
-# also accepts an optional quantity -->  expect{cash_register.add_item("book", 5.00, 3)}.to change{cash_register.total}.by(15.00)
 # doesn't forget about the previous total
