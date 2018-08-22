b65b4d6b556ce09d080da660d14094d1ab266bcd
Rafael Lopez <lpz.raf@gmail.com>
2018-08-22 03:28:50 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,14 +6,13 @@ class CashRegister
     @discount = discount
   end
   
-  # def add_item(title, price, optional_quantity = 1)
-  #   self.total =+ price
-  # end
-  
    def add_item(title, price, quantity = 1)
     self.total = price * quantity
-    total
   end
 end
 
 # doesn't forget about the previous total
+  # cash_register.add_item("Lucky Charms", 4.5)
+  # expect(cash_register.total).to eq(4.5)
+  # cash_register.add_item("Ritz Crackers", 5.0)
+  # expect(cash_register.total).to eq(9.5)
