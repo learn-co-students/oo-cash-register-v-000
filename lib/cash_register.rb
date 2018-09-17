2e52cc29bd3020c7384cbe435704bfd050908253
Ashley Wong <ashwong04@gmail.com>
2018-09-17 01:24:54 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
+class CashRegister
 
+  attr_accessor :total
+  attr_accessor :discount
+
+  def initialize(discount = nil)
+    @total = 0
+    @discount = discount.fdiv(100)
+  end
+
+  def add_item(title, price, quantity=1)
+    self.total += price*quantity
+  end
+
+  def apply_discount
+    self.total *= @discount
+  end
+
+
+end
