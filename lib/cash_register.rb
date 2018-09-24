2953f341f2f2b9d1ed4793ca674c4fc69a260248
Sherry Martin <sm674@cornell.edu>
2018-09-24 16:16:32 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,15 +1,21 @@
 require 'pry'
 
 class CashRegister
-  attr_accessor :total, :discount, :add_item
+  attr_accessor :total, :discount
 
-def initialize(total = 0, discount = 20)
-  @total = total
-  @discount = discount
-end
-  
-  def add_item(title, price)
-    @total = total + price
+  def initialize(total = 0, discount = 20)
+    @total = total
+    @discount = discount
+    @@cart = []
   end
   
+  def add_item (title, price, quantity=1)
+    @total += price * quantity
+    @last_transaction = price * quantity
+    quantity.times do 
+     @@cart << title
+  end
+end
+
+
 end
