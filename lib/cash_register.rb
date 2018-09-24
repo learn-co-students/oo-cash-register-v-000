23b8b8fe809267b653086c2a4f24c96bfcc6b0a4
Sherry Martin <sm674@cornell.edu>
2018-09-24 15:30:24 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
+require 'pry'
 
+class CashRegister
+  attr_accessor :total, :discount, :add_item
+
+def initialize(total = 0, discount = 20)
+  @total = total
+  @discount = discount
+end
+  
+  def add_item(title, price)
+    @total = total + price
+  end
+  
+end
