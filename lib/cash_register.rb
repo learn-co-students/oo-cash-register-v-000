7bca40bf0378270374bf6966a128c2ec54112ac5
Cheryl K <cherylkelson@icloud.com>
2018-09-30 15:26:51 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,58 @@
+require pry 
+
+class CashRegiste
+
+  attr_accessor :items, total, :discount, :last_transaction
+
+
+array [item, price, quantity]
+  cash_register.instance_variable_get(total)
+  @items = []
+  @discount discount 
+  @total = 0 
+  
+
+
+  def initialize
+  @cash_register = cash register
+  @employee_discount = employee discount
+
+  end
+
+
+  def total
+    @cash_register = 100.0
+  
+  end
+  
+
+
+  def add_item(title, price, optional_quantity) 
+    @total += cash_register_price.add_item     .collect {|item| item  = (item, total, optional_quantity) = (total * optional_quantity)
+    
+  end 
+
+  def apply_discount 
+    @total 
+    @totalprice
+    @cash_register_with_discount.add_item = (cash_register_with_discount.total)
+    cash_register_with_discount.apply_discount
+    puts "After the discount, the total comes to $800."
+      if cash_register_with_discount(employee_discount)
+        puts "There is no discount to apply."
+    total =-
+  end 
+
+  def items(array)
+    @@items=add.item
+  
+  end
+
+
+  def void_last_transaction
+   total -last_transaction
+   = cash_register.void_last_transaction - cash_register.total
+  end
+
+
 
