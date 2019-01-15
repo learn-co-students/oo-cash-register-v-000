1acee8e6af537583659671dfc43f32deaca8095c
Niusha H <niusha.hadziomerovic@gmail.com>
2019-01-15 19:46:37 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,30 @@
+class CashRegister
+  attr_accessor :total, :employee_discount, :discount
+
+  def initialize(total = 0)
+    @total = total
+  end
+  
+  def total
+    employee_discount = self.total += 20
+  end
+
+  # def add_item
+
+  # end
+
+  # def apply_discount
+
+  # end
+  
+  # def item
+
+  # end  
+  
+  # def void_last_transaction
+
+  # end
+  
+end  
+
 
