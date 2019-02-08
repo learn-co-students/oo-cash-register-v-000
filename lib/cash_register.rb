608c6696e0f96c6058b5685605648fa97f28757b
Kimberly Aviles <01010110.v@gmail.com>
2019-02-08 02:26:35 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,23 +1,45 @@
 class CashRegister
-  attr_accessor :total, :discount
+  attr_accessor :total, :discount, :items, :last_transaction
 
   def initialize(discount = nil)
     @total = 0
     @discount = discount
+    @items = []
   end
 
   def add_item(key, value, quantity = 1)
-    @total += value * quantity
+    self.total += value * quantity
+
+##################################################
+    # quantity_counter = 0
+    # loop do
+    #   quantity_counter += 1
+    #   self.items << key
+    #   if quantity_counter == quantity
+    #     break
+    #   end
+    # end
+#####################################refactorPLZkTY
+#totally forgot about using the TIMES LOOP - but here's the LEARNsolution:
+    quantity.times do
+      items << key
+    end
+##################################################
+    self.last_transaction = [key, value, quantity]
   end
 
   def apply_discount
     if discount != nil
-      money_off = @total * @discount / 100
-      @total -= money_off
-      return "After the discount, the total comes to $#{@total}."
+      money_off = self.total * self.discount / 100
+      self.total -= money_off
+      return "After the discount, the total comes to $#{self.total}."
     else
       return "There is no discount to apply."
     end
+  end
+
+  def void_last_transaction
+    self.total -= self.last_transaction[1] * self.last_transaction[2]
   end
 
 
