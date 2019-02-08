a5c112793b62ee9fc9e866e1b9e877b5501d00d6
Kimberly Aviles <01010110.v@gmail.com>
2019-02-08 02:29:08 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,14 +7,13 @@ class CashRegister
     @items = []
   end
 
-  def add_item(key, value, quantity = 1)
+  def add_item(item, value, quantity = 1)
     self.total += value * quantity
-
 ##################################################
     # quantity_counter = 0
     # loop do
     #   quantity_counter += 1
-    #   self.items << key
+    #   self.items << item
     #   if quantity_counter == quantity
     #     break
     #   end
@@ -22,10 +21,10 @@ class CashRegister
 #####################################refactorPLZkTY
 #totally forgot about using the TIMES LOOP - but here's the LEARNsolution:
     quantity.times do
-      items << key
+      items << item
     end
 ##################################################
-    self.last_transaction = [key, value, quantity]
+    self.last_transaction = [item, value, quantity]
   end
 
   def apply_discount
