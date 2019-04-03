66022169fe98d8f9ebc8c2ef6d99f952d316577f
Dan Suarez <suarez.daniel101@gmail.com>
2019-04-03 05:49:30 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,17 +1,25 @@
+#require 'pry'
 class CashRegister
-  attr_accessor :total, :discount
+  attr_accessor :total, :disc
 
-  def initialize(total = 0)
+  def initialize(total = 0.0)
     @total = total
-    @discount = discount
+    @disc = disc
 
   end
 
   def discount
-    self.discount = 20
+    self.disc = 20
   end
 
   def add_item(title, price, quantity = 1)
     self.total += price * quantity
   end
+
+  def apply_discount
+    @total/self.discount = self.total 
+
+  end
+#100.class returns Integer while 100.0.class returns Float
+  #binding.pry
 end
