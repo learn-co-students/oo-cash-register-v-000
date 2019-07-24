f917687de3e879d60b8913136323a55e956c6c1e
Morgan VanYperen <morgvanny@gmail.com>
2019-07-24 13:08:05 -0700

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,14 +18,13 @@ class CashRegister
   def apply_discount
     if @discount != 0
       @total = @total * (100 - @discount) / 100
-      "After the discount, the total comes to $#{@total}."
+      "After the discount, the total comes to $#{total}."
     else
-      "There is no discount to apply."
+      'There is no discount to apply.'
     end
   end
-  
-  def void_last_transaction
-    @total = @total - @last_transaction
-  end
 
+  def void_last_transaction
+    @total -= @last_transaction
+  end
 end
