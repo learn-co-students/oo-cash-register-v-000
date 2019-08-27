3ca95922ebfaf2cd35ecfbd5f6c1d264eb9a3b3d
Andrea T <tostonar@gmail.com>
2019-08-27 14:56:34 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,21 +17,22 @@ class CashRegister
     quantity.times do
       @items << title
     end
-    self.total = self.total + (price * quantity)
-    self.last_transaction = price * quantity
+    @last_transaction = price * quantity
+    @total +=  @last_transaction
+    
   end
 
   def apply_discount
-    if discount >= 1
-      self.total = (total - (self.total * (discount.to_f/100))).to_i
-      "After the discount, the total comes to $#{self.total}."
+    if @discount >= 1
+      @total -= (@total * (@discount/100.0)).to_i
+      "After the discount, the total comes to $#{@total}."
     else
       "There is no discount to apply."
     end
   end
 
   def void_last_transaction
-    self.total = self.total - self.last_transaction
+    @total -=  @last_transaction
   end
 
 end
