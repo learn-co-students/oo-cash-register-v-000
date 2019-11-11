06022912d4af538ac4b934da737bbcc6a08783b2
Eva Navarro <emanao@me.com>
2019-11-11 21:08:56 +0000

    Encapsulate instance variable

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,7 @@
 require "pry"
 class CashRegister
   attr_accessor :total, :discount, :quantity, :last_transaction
+  attr_reader :items
 
   def initialize(discount=0)
     @total= 0
@@ -11,26 +12,20 @@ class CashRegister
   def items= (item)
     @items<<item
   end
-  def items
-    @items
-  end
 
   def add_item(item, price, quantity=1)
-    quantity.times do
-      self.items=item
-    end
-    @quantity=quantity
-    @last_transaction = price*quantity
-    @total += price*quantity
+    quantity.times { self.items=item }
+    self.quantity=quantity
+    self.last_transaction = price*quantity
+    self.total += price*quantity
   end
 
   def apply_discount
-    @total -= @total * @discount/100
-    @discount==0?"There is no discount to apply.":"After the discount, the total comes to $800."
+    self.total -= self.total * self.discount/100
+    self.discount==0?"There is no discount to apply.":"After the discount, the total comes to $800."
   end
 
   def void_last_transaction
-     @total -= @last_transaction
+     self.total -= self.last_transaction
   end
-
 end
