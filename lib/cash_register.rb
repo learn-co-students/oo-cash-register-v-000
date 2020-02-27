cdc3deb62cac3eb772961f1cf861200fab1295bf
Erica Murdock-Waters <ericahpa@gmail.com>
2020-02-27 00:55:02 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,23 +1,23 @@
 # cash_register.rb
-
+require "pry"
 class CashRegister
-  attr_accessor :total, :discount, :title, :price, :quantity
+  attr_accessor :total, :discount, :price, :quantity
+  attr_reader :title, :items
 
   def initialize(discount = 0)   #employee discount on initialization optional
     @total = 0
     @discount = discount
     @items = []                  # stores all titles instance of cash_register
-    @transactions = []           # stores all transactions cash_register
+    @line_items = []           # stores all transactions cash_register
   end
 
   def add_item(title, price, quantity = 1)
     @title = title
     @price = price
     @quantity = quantity
-
     @items.fill(@title, @items.size, @quantity)
     self.total += (@price * @quantity)
-    @transactions << self
+    @line_items << self
   end
 
   def apply_discount              # applies the discount to the total price
@@ -27,10 +27,6 @@ class CashRegister
       @total -= @total * @discount / 100
       p "After the discount, the total comes to $#{@total}."
     end
-  end
-
-  def items
-    @items
   end
 
   def void_last_transaction       # subtracts the last transaction from the total
