ca73206df9a858425023d2379e77781fe307e587
Lamia Haider <lamia.haider@gmail.com>
2020-04-17 05:35:45 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,40 +1,41 @@
+require 'pry'
 class CashRegister
-  attr_accessor :total, :discount, :items, :last_transaction
+  attr_accessor :discount, :total, :items, :last_transaction
 
-  def initialize (discount = 0)
+  def initialize(discount=0) #because this is optional we're setting a default value that can be overriden
     @total = 0
-    @discount = discount
+    @discount = discount #otherwise discount will be nil
     @items = []
   end
-  
-  #If we want to void last transaction we need to remove the last item from the array and it's value from the total.
-  
-   def add_item (title, price, quantity=1)
-     self.total += (price * quantity) 
-     quantity.times do
-       @items << title
-     end
-     self.last_transaction = (price*quantity)
-   end
 
+  def total
+    @total
+  end
+
+  def add_item(title, price, quantity=1)
+    @total = @total + (price * quantity)
+    quantity.times do
+      @items << title #wants repeat items
+    end
+    self.last_transaction = price*quantity #build this into this method since the added item will be the most recent transaction
+  end
 
   def apply_discount
-    if discount > 0
-    self.total = (total - (total * (discount/100.0))).to_i
-      return "After the discount, the total comes to $#{self.total}."
+    if @discount > 0
+      @total = (@total - ((@discount/100.0) * @total)).to_i
+      return "After the discount, the total comes to $#{@total}."
     else
       return "There is no discount to apply."
     end
   end
-  
+
   def items
     @items
   end
-  
+
   def void_last_transaction
-    self.total -= last_transaction
-  end 
+    @total -= self.last_transaction
+  end
+
 
 end
-
-
