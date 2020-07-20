ca5d96a23826a889689638b48b25502ca052a0a7
hkato4188 <hkato@sas.upenn.edu>
2020-07-20 17:22:58 -0400

    refactored CashRegister instance methods

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,37 +2,32 @@ require 'pry'
 
 class CashRegister
 
-  attr_accessor :total, :discount, :last_transaction
+  attr_accessor :items, :discount, :total, :last_transaction
 
-  def initialize(discount=0)
-    @total = 0
-    @discount = discount
-    @items = []
+  def initialize(employee_discount=nil)
+      @total = 0
+      @discount = employee_discount
+      @items = []
   end
 
   def add_item(title, price, quantity=1)
-    self.total += price * quantity
-    quantity.times do
-      @items << title
-    end
-    @last_transaction = price * quantity
+      self.total += price * quantity
+      quantity.times do
+        @items << title
+      end
+      self.last_transaction = price * quantity
   end
 
   def apply_discount
-    if discount == 0
-      return "There is no discount to apply."
+    if @discount
+      self.total -= (@total * @discount/100.to_f).to_i
+      "After the discount, the total comes to $#{self.total}."
     else
-      self.total = (total * (1 - (discount.to_f / 100))).to_i
-      return "After the discount, the total comes to $#{self.total}."
+      "There is no discount to apply."
     end
   end
 
-  def items
-    @items
-  end
-
   def void_last_transaction
-    self.total = @total - @last_transaction
+    self.total -= @last_transaction
   end
-
 end
